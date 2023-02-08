993abcd538d6a74e0a0b009fcfa16eb10a179000
aegilops <41705651+aegilops@users.noreply.github.com>
2023-02-08 15:36:18 +0000

    Fix input truncation 🐞

--- edit_sarif.py
+++ edit_sarif.py
@@ -9,6 +9,7 @@ from enum import Enum
 import json
 import argparse
 import logging
+import sys
 from typing import Any
 
 LOG = logging.getLogger(__name__)
@@ -57,7 +58,8 @@ def add_args(parser: argparse.ArgumentParser) -> None:
         help='List of Rule IDs to edit, with sub-severity, comma-separated, e.g. py/code-injection,low'
     )
     parser.add_argument('--custom-tags', '-t', type=lambda arg: arg.split(",", maxsplit=2), nargs='+', help='List of custom tags to add to each rule')
-    parser.add_argument('sarif', help='SARIF file to edit')
+    parser.add_argument('input-sarif', help='SARIF file to edit')
+    parser.add_argument('--output-sarif', '-o', help='SARIF file to write')
     parser.add_argument('--debug', '-d', action='store_true', help='Enable debug logging')
 
 
@@ -72,22 +74,23 @@ def main() -> None:
     if args.debug:
         LOG.setLevel(logging.DEBUG)
 
-    with open(args.sarif, 'r') as f:
+    with open(args.input_sarif, 'r') as in_f:
         try:
-            data = f.read()
+            data = in_f.read()
             sarif = json.loads(data)
         except json.JSONDecodeError as err:
             LOG.error("Error parsing SARIF file as JSON (%s): %s", args.sarif, err)
             LOG.error("Could not parse as JSON: %s", data)
             return
-        print(json.dumps(
-            edit_sarif(
-                sarif,
-                args.rule_id_severity if args.rule_id_severity else [],
-                args.custom_tags if args.custom_tags else []
-            )
-            , indent=2
-        ))
+        # NOTE: closes STDOUT after the context manager if no output file is specified
+        with open(args.output_sarif, 'w') if args.output_sarif else sys.stdout as out_f:
+            print(json.dumps(
+                edit_sarif(
+                    sarif,
+                    args.rule_id_severity if args.rule_id_severity else [],
+                    args.custom_tags if args.custom_tags else []
+                ), indent=2)
+                , file=out_f)
 
 
 if __name__ == "__main__":
