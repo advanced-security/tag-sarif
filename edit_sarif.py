b40abd7f9ce4b5155a0c6e9c86beff6d40badf63
aegilops <41705651+aegilops@users.noreply.github.com>
2023-02-08 15:36:33 +0000

    Fix argument parser errors

--- edit_sarif.py
+++ edit_sarif.py
@@ -57,8 +57,8 @@ def add_args(parser: argparse.ArgumentParser) -> None:
         type=lambda arg: arg.split(",", maxsplit=2), nargs='+',
         help='List of Rule IDs to edit, with sub-severity, comma-separated, e.g. py/code-injection,low'
     )
-    parser.add_argument('--custom-tags', '-t', type=lambda arg: arg.split(",", maxsplit=2), nargs='+', help='List of custom tags to add to each rule')
-    parser.add_argument('input-sarif', help='SARIF file to edit')
+    parser.add_argument('--custom-tags', '-t', type=lambda arg: arg.split(",", maxsplit=2), help='List of custom tags to add to each rule')
+    parser.add_argument('input_sarif', help='SARIF file to edit')
     parser.add_argument('--output-sarif', '-o', help='SARIF file to write')
     parser.add_argument('--debug', '-d', action='store_true', help='Enable debug logging')
 
